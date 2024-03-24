01a49d8c9eb438703a610710ba9ef4d4ba8e2acd
Hannah <hnarme@ymail.com>
2024-03-24 23:46:51 +0000

    Created react portfolio website
    Created and edited portfolio

---
+++ portfoliowebsite-react/src/components/About.js
@@ -0,0 +1,31 @@
+export default function About() {
+    return (
+        <section
+            id="about"
+            className="px-10 w-full flex flex-col lg:flex-row py-20 
+                       align-center bg-emerald-200 max-w-5xl mx-auto">
+            <div className="flex-1">
+                <img src="src\images\me.jpg"
+                    alt="About"
+                    className="w-full h-full bg-cover"/>
+            </div>
+            <div className="flex-1 flex flex-col justify-center
+                            items-center gap-5 px-6">
+                <div>
+                    <h2 className="text-center text-emerald-500 
+                                   text-5xl font-bold">
+                        About Me :
+                    </h2>
+                </div>
+                <p>
+                    I am a computer science graduate looking to get a job as a software developer...
+                </p>
+                <p>
+                    blahahahaha
+
+                    
+                </p>
+            </div>
+        </section>
+    );
+}
