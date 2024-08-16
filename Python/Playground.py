83fa314830590e66e980b773f0098119b306ec05
Hannah <hnarme@ymail.com>
2024-08-16 15:27:33 +0100

    Cleared python playground script
    Saved the current script in the playground script to clear it. Created a script called ArraySelectPrint which prints a range of elements in an index in an array or not

--- Python/Playground.py
+++
@@ -1,9 +0,0 @@
-colors = ["red", "red", "green", "blue"]
-sampleText = 'Name Ricardo Darín'
-# your code goes here
-for color in colors:
-    if color == color:
-        continue
-    print(color)
-
-print(sampleText[5:12])
