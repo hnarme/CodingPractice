d245212661c59ae4781b595df24aba95b36ec04a
Hannah <hnarme@ymail.com>
2024-08-28 13:42:51 +0100

    csharp althaus folder added
    folder for althaus

---
+++ C#/althaus/MyfirstProject/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int delay = 1000;
+
+Console.WriteLine("Computer loading.");
+Thread.Sleep(delay);
+Console.WriteLine("Computer loading..");
+Thread.Sleep(delay);
+Console.WriteLine("Computer loading...");
+Thread.Sleep(delay);
+Console.WriteLine("Computer loading....");
+Thread.Sleep(delay);
+Console.WriteLine("Load complete");
