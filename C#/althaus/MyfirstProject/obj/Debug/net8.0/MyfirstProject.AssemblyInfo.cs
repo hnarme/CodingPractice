56bf4d47a6337927938a9491bf25a5ce5ef2e1c0
Hannah <hnarme@ymail.com>
2024-08-30 15:03:59 +0100

    Program update adventure game

--- C#/althaus/MyfirstProject/obj/Debug/net8.0/MyfirstProject.AssemblyInfo.cs
+++ C#/althaus/MyfirstProject/obj/Debug/net8.0/MyfirstProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyfirstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70bd2da4a89904ed408a900fcbceb37a1d3b6a65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dfe8c102e5e81c1e13634cc79916737761de8d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyfirstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyfirstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
