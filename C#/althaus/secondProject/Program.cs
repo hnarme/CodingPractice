f5ddc58523922091091d2f8c3eeac9e110bbe739
hnarme <hnarme@ymail.com>
2024-09-04 12:03:46 +0100

    Update Program.cs

--- C#/althaus/secondProject/Program.cs
+++ C#/althaus/secondProject/Program.cs
@@ -51,7 +51,7 @@ foreach (int myInt in myInts)
     Console.WriteLine(myInts);
 } */
 
-List<string> fruits;
+/* List<string> fruits;
 fruits = new List<string>();
 fruits[0] = "Apple";
 fruits[1] = "Banana";
@@ -60,15 +60,42 @@ fruits[2] = "Cherry";
 foreach (string myString in fruits)
 {
     Console.WriteLine(myString);
-}
+} */
 
-do
+/* do
 {
     Console.WriteLine(fruits);
 }
-while (fruits.Count > 0);
+while (fruits.Count > 0); */
 
-while (fruits.Count > 0)
+/* while (fruits.Count > 0)
 {
     Console.WriteLine(fruits + "while loop");
+} */
+
+/* string input;
+string[] fruits = new string[] {}
+do
+{
+    console.WriteLine("Please type a fruit!");
+    input = Console.ReadLine();
+    fruits.Add(input);
+}
+while(input != "quit"); */
+
+List<string> fruits = new List<string>();
+string fruitEntered;
+do
+{
+  Console.WriteLine("Please enter a number between the name of a fruit:");
+  fruitEntered = Console.ReadLine();
+  if (fruitEntered != "quit")
+  {
+    fruits.Add(fruitEntered);
+  }
+} while (fruitEntered != "quit");
+// Output all fruits in the list named "fruits"
+foreach (string fruit in fruits)
+{
+  Console.WriteLine(fruit);
 }
