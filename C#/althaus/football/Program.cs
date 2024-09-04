a3f243fbf7e10ba6eeeaa69291c7b83aa2ad5148
Hannah <hnarme@ymail.com>
2024-09-04 09:35:30 +0100

    New project football choice

---
+++ C#/althaus/football/Program.cs
@@ -0,0 +1,13 @@
+﻿string choice = "";
+
+Console.WriteLine("What is your favourite football team?");
+choice = Console.ReadLine();
+
+if (choice == "Birmingham City")
+{
+    Console.WriteLine("Yeah! Awesome!");
+}
+else if (choice == "")
+{
+
+}
