834541fd83e0bd68968b705d13d652ec523fb1f5
Hannah <hnarme@ymail.com>
2024-09-04 10:06:31 +0100

    C# arays program

---
+++ C#/althaus/secondProject/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* Console.WriteLine("Hello, World!");
+int[] myData = new int[5];
+myData[0] = 67;
+myData[1] = 45;
+myData[2] = 34;
+myData[3] = 36;
+myData[4] = 42;
+myData[5] = 45;
+int[] myData2 = new int[] { 54, 32, 11, }; */
+
+string[] fruits = new string[2];
+fruits[0] = "Apple";
+fruits[1] = "Banana";
+fruits[2] = "Cherry";
+Console.WriteLine(fruits[0]);
+fruits[2] = "Elderberry";
+Console.WriteLine(fruits[2]);
+
