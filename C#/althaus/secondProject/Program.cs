e363f421182531f84da613e8fede5b597b406bee
Hannah <hnarme@ymail.com>
2024-09-04 10:38:22 +0100

    for loops c#

--- C#/althaus/secondProject/Program.cs
+++ C#/althaus/secondProject/Program.cs
@@ -9,11 +9,37 @@ myData[4] = 42;
 myData[5] = 45;
 int[] myData2 = new int[] { 54, 32, 11, }; */
 
-string[] fruits = new string[2];
+/* string[] fruits = new string[2];
 fruits[0] = "Apple";
 fruits[1] = "Banana";
 fruits[2] = "Cherry";
 Console.WriteLine(fruits[0]);
 fruits[2] = "Elderberry";
-Console.WriteLine(fruits[2]);
+Console.WriteLine(fruits[2]); 
+fruits[2] = "Cherry";
+Console.WriteLine(fruits[3]); */
 
+/* List<int> fruits2;
+fruits2 = new List<int>();
+fruits2.Add(5);
+fruits2[0] = 3;
+fruits2[1] = 4;
+fruits2[2] = 5;
+fruits2[3] = 6;
+fruits2[4] = 7; */
+
+/* List<string> fruits;
+fruits = new List<string>();
+fruits[0] = "Apple";
+fruits[1] = "Banana";
+fruits[2] = "Cherry";
+Console.WriteLine(fruits[1]);
+fruits[2] = "Elderberry";
+Console.WriteLine(fruits[2]);
+fruits.Add("Durian");
+Console.WriteLine(fruits[3]); */
+
+for (int myInt = 0; myInt < 10; myInt++)
+{
+    Console.WriteLine(myInt);
+}
