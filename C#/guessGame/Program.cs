c1fa799b3ef540b8e493aff7ea4157efdbccf58c
Hannah <hnarme@ymail.com>
2024-09-05 13:06:06 +0100

    Created Guess Game program

---
+++ C#/guessGame/Program.cs
@@ -0,0 +1,13 @@
+﻿//Guessing game where you have to guess the number 
+
+int computerNumber = 5;
+int guessNum = 3;
+int guessChoice;
+
+do
+{
+    Console.WriteLine("Try to guess the number. You win if you get it right and have 3 guesses. Good luck!");
+    Console.WriteLine("Type a number between 1-50");
+    Console.ReadLine();
+}
+while (guessNum != 0);
