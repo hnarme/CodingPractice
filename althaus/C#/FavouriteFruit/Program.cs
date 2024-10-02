253158d5ed896b0f42bc48c099d7abe3b69405d2
Hannah <hnarme@ymail.com>
2024-10-02 09:44:15 +0100

    Update Program.cs

--- althaus/C#/FavouriteFruit/Program.cs
+++ althaus/C#/FavouriteFruit/Program.cs
@@ -1,9 +1,9 @@
 ﻿Person person = new Person();
 
-Console.WriteLine("Please type your name!");
-person.Name = Console.ReadLine();
-Console.WriteLine("Please type your favourite fruit!");
-person.FavouriteFruit = Console.ReadLine();
+//Console.WriteLine("Please type your name!");
+//person.Name = Console.ReadLine();
+//Console.WriteLine("Please type your favourite fruit!");
+//person.FavouriteFruit = Console.ReadLine();
 
 Console.Write(person.DisplayGreeting());
 
