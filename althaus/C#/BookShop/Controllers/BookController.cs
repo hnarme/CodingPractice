1d45bd4939c7dab735a73c24987935a9152ce5b5
Hannah <hnarme@ymail.com>
2024-10-02 11:01:06 +0100

    bookshop update

--- althaus/C#/BookShop/Controllers/BookController.cs
+++ althaus/C#/BookShop/Controllers/BookController.cs
@@ -18,11 +18,6 @@ public class BookController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
