da914e6b1fb4b933c5d16f5811052b27d8ddae15
Hannah <hnarme@ymail.com>
2024-10-04 13:18:25 +0100

    update api

--- althaus/C#/API/Program.cs
+++ althaus/C#/API/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+HttpClient client = new HttpClient();
+HttpResponseMessage response = await client.GetAsync("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=bf5f5026-1766-49c3-9d9f-aff7ae95a8e6");
+string sitelist = await response.Content.ReadAsStringAsync();
+dynamic sitelistJson = JsonConvert.DeserializeObject(sitelist);
+Console.WriteLine(sitelistJson.Locations.Location[0].name);
