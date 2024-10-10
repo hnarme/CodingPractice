d90775073833b2781107ba87c954cb6511d72f48
Hannah <hnarme@ymail.com>
2024-10-10 11:35:01 +0100

    Created dentist system and sql commands in C file
    Started dentist appointment system project and updated C file with all sql commands saved from the youtube video

---
+++ althaus/DentistSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DentistSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
