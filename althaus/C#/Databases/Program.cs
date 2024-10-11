9f6b7d04e7e7aa767c9951f94ce6b2a1667dbd3c
Hannah <hnarme@ymail.com>
2024-10-11 10:16:48 +0100

    Databse working

--- althaus/C#/Databases/Program.cs
+++ althaus/C#/Databases/Program.cs
@@ -1,16 +1,29 @@
-﻿MySql.Data.MySqlClient.MySqlConnection conn;
+﻿using MySql.Data.MySqlClient;
+
+MySql.Data.MySqlClient.MySqlConnection conn;
 string myConnectionString;
 
 myConnectionString = "server=127.0.0.1;uid=root;" +
     "pwd=root;database=airbnb";
-
+conn = new MySql.Data.MySqlClient.MySqlConnection();
 try
 {
-    conn = new MySql.Data.MySqlClient.MySqlConnection();
     conn.ConnectionString = myConnectionString;
+    Console.WriteLine("Connecting to MySQL...");
     conn.Open();
+
+    string sql = "SELECT * FROM User";
+    MySqlCommand cmd = new MySqlCommand(sql, conn);
+    MySqlDataReader rdr = cmd.ExecuteReader();
+
+    while (rdr.Read())
+    {
+        Console.WriteLine(rdr[0] + " -- " + rdr[1]);
+    }
+    rdr.Close();
 }
 catch (MySql.Data.MySqlClient.MySqlException ex)
 {
     Console.WriteLine(ex.Message);
 }
+conn.Close();
