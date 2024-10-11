f3cf0ee69b56e79279336ee71e72a38a911b7182
Hannah <hnarme@ymail.com>
2024-10-11 10:06:58 +0100

    Database connection working

--- althaus/C#/Databases/Program.cs
+++ althaus/C#/Databases/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿MySql.Data.MySqlClient.MySqlConnection conn;
+string myConnectionString;
+
+myConnectionString = "server=127.0.0.1;uid=root;" +
+    "pwd=root;database=airbnb";
+
+try
+{
+    conn = new MySql.Data.MySqlClient.MySqlConnection();
+    conn.ConnectionString = myConnectionString;
+    conn.Open();
+}
+catch (MySql.Data.MySqlClient.MySqlException ex)
+{
+    Console.WriteLine(ex.Message);
+}
