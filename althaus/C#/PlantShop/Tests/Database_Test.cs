d3bb919d16e4a493413c3f952bbae0df4c92813a
Hannah <hnarme@ymail.com>
2024-10-25 12:55:43 +0100

    created add, get and delete test method

--- althaus/C#/PlantShop/Tests/Database_Test.cs
+++ althaus/C#/PlantShop/Tests/Database_Test.cs
@@ -16,4 +16,27 @@ public class Database_Test
             Assert.Fail(ex.Message);
         }
     }
+
+    [TestMethod]
+    public void AddGetAndDeletePlantOnDatabase()
+    {
+        Plant plant = new Plant();
+        plant.Name = "TestMethod";
+        plant.family = "Test";
+        Database.Instance.AddPlant(plant);
+        List<Plant> plants = Database.Instance.GetAllPlant();
+        bool found = false;
+        foreach(Plant plant in plants)
+        {
+            if(plant.Name == "TestMethod" && plant.Family == "Test")
+            {
+                found = true;
+            }
+            if(!found)
+            {
+                Assert.Fail("Plant not found");
+            }
+            Database.Instance.DeletePlant(plant);
+        }
+    }
 }
